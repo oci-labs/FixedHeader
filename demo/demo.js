6a4df40364111917c9d0c5dde0495c15c9d36e79
LMFinney <lmfinney@alum.wustl.edu>
2014-02-28 14:57:01 -0600

    Added copyright notices and inline comments to the javascript code. Arbitrarily set the version to 0.1.0.

--- demo/demo.js
+++ demo/demo.js
@@ -1,1 +1,7 @@
+/*
+ @license OCI Fixed Header version 0.1.0
+ ⓒ 2014 OCI https://github.com/objectcomputing/FixedHeader
+ License: MIT
+ */
+
 angular.module('app', ['oci.fixedHeader']);
--- dist/fixed-header.js
+++ dist/fixed-header.js
@@ -1,14 +1,25 @@
+/*
+ @license OCI Fixed Header version 0.1.0
+ ⓒ 2014 OCI https://github.com/objectcomputing/FixedHeader
+ License: MIT
+ */
+
 (function () {
     'use strict';
 
     angular.module('oci.fixedHeader', [])
         .directive('oci.fixedHeader', function () {
             return function link(scope, elem) {
+                // Wrap the contents of every header cell with div.th-inner so that the
+                // CSS can relocate it.
                 var header = elem.find('thead').find('tr');
                 angular.forEach(header.find('th'), function (th) {
                     angular.element(th).contents().wrap('<div class="th-inner"></div>');
                 });
 
+                // Make a clone of the header that we hide using css. The purpose of this
+                // is to allow the width of the contents of the header to be included
+                // in the calculation of the widths of the columns
                 var hiddenHeader = header.clone();
                 hiddenHeader.addClass('hidden-header');
                 header.after(hiddenHeader);
